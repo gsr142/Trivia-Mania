e53e3715c45af5afc78b0e7a5233828a1c522db5
justinschoi93 <justinschoi93@gmail.com>
2023-08-12 16:31:58 -0700

    trivapage and game.js started

---
+++ public/js/game.js
@@ -0,0 +1,9 @@
+document.querySelector('.answer-a').addEventListener('click', checkAnswer);
+document.querySelector('.answer-b').addEventListener('click', checkAnswer);
+document.querySelector('.answer-c').addEventListener('click', checkAnswer);
+document.querySelector('.answer-d').addEventListener('click', checkAnswer);
+
+const checkAnswer = async (req, res) => {
+
+
+}
