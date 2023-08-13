d86414428c26d3fe3f3d4d228298394eca39ce4a
justinschoi93 <justinschoi93@gmail.com>
2023-08-12 18:53:50 -0700

    pseudocode revised. I think we might be reaady to start coding this part out.

--- controllers/api/gameRoutes.js
+++ controllers/api/gameRoutes.js
@@ -1,10 +1,20 @@
 const router = require('express').Router();
 const { Deck,  Question} = require('../../models');
 
-router.use('/:category/:difficulty', {
+//fetch will be made upon category and difficulty getting selected
+//GET: api/:category/:difficulty
+router.get('/:category/:difficulty', async (req, res) => {
+    //db.query to use the category req param to get a category id. save to const cat_id
+    const cat_id = db.query(`SELECT id FROM category WITH name = ?`, req.params.category, (err, res) => {
+        return res;
+    });
 
-    const questionsData = Question.findAll({include: {where: {
-        categery: req.params.category,
+    const questionsJSON = await Question.findAll({include: {where: {
+        categery_id: cat_id,
         difficulty: req.params.difficulty,
     }}})
+
+    // serialize questionsJSON, save to const questions
+
+    // Deck.bulkCreate(questions)
 })
