872f9d94ab347a9ad58d6b61333dd91496ba3535
Joel <joellongaresjr@gmail.com>
2023-08-14 03:26:19 -0700

    couldn't get questionare to pop up, will restart efforts tomorrow, added css styling, and added some direction on questionare portion of this application

--- public/js/triviapage.js
+++ public/js/triviapage.js
@@ -26,15 +26,38 @@ document.addEventListener('DOMContentLoaded', () => {
   document
     .getElementById('start-trivia-button')
     .addEventListener('click', async () => {
+      console.log('Start Trivia button clicked'); // Added console log
+
       const categorySelect = document.getElementById('category-select');
       const difficultySelect = document.getElementById('difficulty-select');
 
       const selectedCategoryId = categorySelect.value;
       const selectedDifficulty = difficultySelect.value;
+// below is optional, i tried console logging stuff but it wasn't doing anything for me here
+      try {
+        console.log('Sending request...');
+        const response = await fetch(`/api/questions/${selectedCategoryId}?difficulty=${selectedDifficulty}`, {
+          method: 'GET',
+        });
 
-      fetchTrivia(selectedCategoryId, selectedDifficulty);
+        if (response.ok) {
+          console.log('Response received:', response);
+          const questions = await response.json();
+          console.log('Questions:', questions);
+          renderQuestions(questions);
+        } else {
+          console.error('Failed to fetch questions');
+        }
+      } catch (error) {
+        console.error('Error fetching questions:', error);
+      }
     });
 });
+      // this portion is here, if we would like to acquire the id of the cateogry
+      // along with the difficulty id
+      // this will prepare for the next questionaire depending on what the user selected
+      // still trying to see if it's better to add difficulty id
+
 
 // dynamically fetching categories
 // overall provides scalability for when we want to add more categories in the future for this application
