f71bfb2f212cdb7d7ccb2f3a1eca9ba9f0d7cb6d
gsr142 <gsr142@gmail.com>
2023-08-14 20:29:09 -0700

    display 5 questions with answers attached to radar selectors

--- public/js/triviapage.js
+++ public/js/triviapage.js
@@ -29,7 +29,7 @@ const getQuestions = async (event) => {
 
   const response = await fetch(`api/question/${category}/${difficulty}`, {
     method: 'GET',
-    headers: { 'Content-type': 'application/json' }
+    headers: { 'Content-type': 'application/json' },
   });
   // console.log(response);
   if (response.ok) {
@@ -37,7 +37,7 @@ const getQuestions = async (event) => {
     const questions = await response.json();
     console.log(questions);
     // return questions
-    populateQuestions(questions)
+    populateQuestions(questions);
   } else {
     console.error(error);
   }
@@ -45,19 +45,48 @@ const getQuestions = async (event) => {
   // return data.render()
 };
 
-async function populateQuestions (questionData){
+async function populateQuestions(questionData) {
   
-  const questionElement = document.getElementById('question');
-  questionElement.textContent = await questionData[0].question_text;
-  const answerA = document.getElementById('answer-A');
-  answerA.textContent = await questionData[0].answer_options.a;
-  const answerB = document.getElementById('answer-B');
-  answerB.textContent = await questionData[0].answer_options.b;
-  const answerC = document.getElementById('answer-C');
-  answerC.textContent = await questionData[0].answer_options.c;
-  const answerD = document.getElementById('answer-D');
-  answerD.textContent = await questionData[0].answer_options.d;
-  
+  for (let i = 0; i < questionData.length; i++) {
+    const gameBoard = document.createElement('div');
+    const questionElement = document.createElement('div');
+    questionElement.setAttribute('class', 'question-panel');
+    questionElement.setAttribute('id', 'question');
+    questionElement.innerText = questionData[i].question_text;
+    
+    const answerPanel = document.createElement('form');
+    answerPanel.setAttribute('class', 'answers-panel');
+
+    const answersList = document.createElement('ul');
+    answersList.setAttribute('class', 'answers-list');
+    categoryCard = document.querySelector('.category-selection')
+    categoryCard.appendChild(gameBoard)
+    gameBoard.appendChild(questionElement);
+    gameBoard.appendChild(answerPanel);
+    answerPanel.appendChild(answersList);
+    
+    
+    let answerArr = questionData[i].answer_options;
+    let answerKey = Object.values(answerArr);
+    
+    answerKey.forEach((elem) => {
+      const answer = document.createElement('li');
+      answer.setAttribute('class', 'answer');
+      
+      const answerInput = document.createElement('input');
+      answerInput.setAttribute('type', 'radio');
+      answerInput.setAttribute('name', 'answer');
+      // answerInput.setAttribute('id', );
+      const label = document.createElement('label');
+      label.setAttribute('for', elem);
+      label.innerText = elem;
+      
+      
+      answer.appendChild(answerInput);
+      answer.appendChild(label);
+      answersList.appendChild(answer);
+    });
+  }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -68,31 +97,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const startTriviaButton = document.querySelector('#start-trivia-button');
   startTriviaButton.addEventListener('click', getQuestions);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // // Function to fetch and populate categories in the dropdown
 // const populateCategories = async () => {
@@ -117,29 +121,26 @@ document.addEventListener('DOMContentLoaded', () => {
 // };
 // document.addEventListener('DOMContentLoaded', () => {
 //   populateCategories();
-  
+
 //   const selectedCategoryId = categorySelect.value;
 //   const selectedDifficulty = difficultySelect.value;
-  
+
 //   const categorySelect = document.getElementById('category-select');
 //   const difficultySelect = document.getElementById('difficulty-select');
 
-
 // const getQuestions = async (event) => {
 //   event.preventDefault();
-  
+
 //   const response = await fetch(`api/question/${category}/${difficulty}`, {
 //       method: 'GET',
 //       headers: { 'Content-type': 'application/json'}
-//   }) 
-  
+//   })
+
 //   const data = await response.json()
 //   console.log(data);
 // }
 
 // const test = document.querySelector('.start-trivia-button').addEventListener('click', getQuestions)
-
-
 
 // // eventlistener responsible for loading content to our dropdown menu calling our function (populateCategories)
 
@@ -173,11 +174,10 @@ document.addEventListener('DOMContentLoaded', () => {
 //       }
 //     });
 // });
-      // this portion is here, if we would like to acquire the id of the cateogry
-      // along with the difficulty id
-      // this will prepare for the next questionaire depending on what the user selected
-      // still trying to see if it's better to add difficulty id
-
+// this portion is here, if we would like to acquire the id of the cateogry
+// along with the difficulty id
+// this will prepare for the next questionaire depending on what the user selected
+// still trying to see if it's better to add difficulty id
 
 // dynamically fetching categories
 // overall provides scalability for when we want to add more categories in the future for this application
