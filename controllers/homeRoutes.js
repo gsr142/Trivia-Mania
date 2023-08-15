d6fb17ad128830c2ba1bc40e12e41451474db6fa
Justin 석원 Choi <justinschoi93@gmail.com>
2023-08-15 11:14:25 -0700

    reverseBubbleSort adjusted to read highscores, and modify order of users

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { Category } = require('../models');
+const { Category, User} = require('../models');
 
 // rendering to homepage
 router.get("/", async (req, res) => {
@@ -13,9 +13,20 @@ router.get("/", async (req, res) => {
 });
 
 router.get("/triviapage", async (req, res) => {
+
     const categoriesData = await Category.findAll();
     const categories = await categoriesData.map(category => category.get({plain: true}));
     res.render('triviapage', {categories: categories, logged_in: req.session.logged_in, high_score: req.session.high_score});
+})
+
+router.get("/leaderboard", async (req, res) => {
+
+    const highScoresData = await User.findAll({attributes: ['name', 'highscore']});
+    const highScores = highScoresData.map( highScore => highScore.get({plain:true}));
+
+    const topThree = reverseBubbleSort(highScores);
+
+    
 })
 
 module.exports = router;
--- utils/helpers.js
+++ utils/helpers.js
@@ -2,8 +2,8 @@ function reverseBubbleSort(arr){
 
     for (let i = arr.length -1; i > 0; i--) {
 
-        if (arr[i] > arr[i-1]) {
-            let a = arr[i];
+        if (arr[i].highscore > arr[i-1].highscore) {
+            let a = arr[i]
             let b = arr[i+1];
 
             arr[i] = b;
